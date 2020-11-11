e90964a96d73b3a8fa3ae2901ebbdfa4d6e1320b
Adam R Bell <a_0x07@protonmail.ch>
2020-11-10 21:00:35 -0700

    Minor correction in the comments...

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,29 @@
  *      Released under MIT License.
  */
 
-
 use std::{thread, time, process, fs};
 
 fn main() {
     println!("Run `sudo strace -p {}`.\n", process::id());
     loop {
-        // This section will show as "stat" things in strace.
+        // This section will show as "stat" things in strace
         println!("Stating some things...");
         fs::metadata("/etc/passwd");
         fs::metadata("/root");
         fs::metadata("/usr/bin/nc");
         fs::metadata("/usr/lib/doesnt-exist");
-        // Sleep for 5 seconds.
+
+        // Sleep for 5 seconds
         thread::sleep(time::Duration::from_millis(5000));
 
-        // This section will show as "open" things in strace.
+        // This section will show as "open" and "read" in strace
         println!("Opening some things...");
         fs::read("/usr/bin/ssh");
         fs::read("/etc/resolv.conf");
         fs::read("/should-be-nothing");
         fs::read(".bashrc");
-        // Sleep for 5 seconds.
+
+        // Sleep for 5 seconds
         thread::sleep(time::Duration::from_millis(5000));
     }
 }
